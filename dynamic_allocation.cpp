2b5a97ddd7f75b77dc150d84d4bf15add2a31966
pskanade <pskanade@users.noreply.github.com>
2017-10-10 11:30:50 +0530

    update

--- dynamic_allocation.cpp
+++ dynamic_allocation.cpp
@@ -16,14 +16,13 @@ int allocateMemory(int *mat){
 	// need to return the address of the pointer to the array
 }
 */
-
-
 int** newalloc(int **m){
 
 	// in C
 	// m = (int **)malloc(sizeof(int*) * 3);
 	//for (int i = 0; i < 3; i++){
 	//	m[i] = (int *)malloc(sizeof(int)* 3);
+	// ** malloc returns a void pointer and hence has to to be type casted to respective type
 	//}
 
 	m = new (int*)(new int(sizeof(int*)* 3));
@@ -48,9 +47,25 @@ int** newalloc(int **m){
 	return m;
 }
 
-int main(){
+/*
+	* Array of pointers : int *mat[3]
+	* pointer to array : int (*mat)[3]
+*/
+
+int ** correctAlloc(int ** m){
+	// array of pointers
+	// it is must to initialize
+	m = new int*[3];
+	for (int i = 0; i < 3; i++)
+		// pointer to array
+		m[i] = new int[3];
+
+	return m;
+}
+
+int main6(){
 	int **mat = NULL;
 	mat = newalloc(mat);
 	cout << mat[0][0] << endl;
 	return 0;
-}
+} 
